4e18e7827fceabcf845e3f164ddbf65be87d86b0
rojo2530 <jcantosg@gmail.com>
2019-06-10 00:27:28 +0200

    Añado fichero main.js para vue

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const vm = new Vue({
+    el:'#app',
+    data: {
+        persona: {
+            nombre: 'Juan',
+            profesoin:'dev',
+            ciudad: 'Cádiz'
+        }
+    }
+
+
+});
