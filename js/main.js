fc51d696950f7dcbcb8a28f7a3170304ddff0835
rojo2530 <jcantosg@gmail.com>
2019-06-10 00:46:54 +0200

    Añado cambios necesarios y cargamos los datos con la API de swapi de las peliculas via axios

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
+
+const url = 'https://swapi.co/api/films/?format=json';
+
 const vm = new Vue({
     el:'#app',
     data: {
-        persona: {
-            nombre: 'Juan',
-            profesoin:'dev',
-            ciudad: 'Cádiz'
+        textSearch: "",
+        films: []
+    },
+    methods: {
+        loadFilms() {
+            axios.get(url)
+                .then((response) => {
+                    this.films = response.data.results;
+                });
         }
-    }
+    },
+    mounted() {
+        // When the Component is ready fetch the JSON from the Server Backend
+        this.loadFilms();
+    },
+
 
 
 });
